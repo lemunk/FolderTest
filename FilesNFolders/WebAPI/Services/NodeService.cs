cd806ce0639586fe271c0e124019350cb4fb85a6
lemunk <munk4224@hotmail.com>
2021-01-31 12:57:08 +0000

    quick implimentation of methods needed

--- FilesNFolders/WebAPI/Services/NodeService.cs
+++ FilesNFolders/WebAPI/Services/NodeService.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebAPI.Models;
 using WebAPI.Services.Interface;
+using WebAPI.ViewModels;
 
 namespace WebAPI.Services
 {
     public class NodeService : INodeService
     {
+        public Guid? CreateNode(NodeViewModel nodeViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Node> ListChildren(Guid parentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Node> ListParents(Guid childId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
