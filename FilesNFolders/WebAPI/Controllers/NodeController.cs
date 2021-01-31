d0da8c99866df94203f1009dc348ee910d158e61
lemunk <munk4224@hotmail.com>
2021-01-31 12:42:04 +0000

    Create first iteration API

---
+++ FilesNFolders/WebAPI/Controllers/NodeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WebAPI.Models;
+using WebAPI.ViewModels;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NodeController : ControllerBase
+    {
+        [HttpGet("Children/{Id}")]
+        public ActionResult<List<Node>> Children(Guid Id)
+        {
+            //todo
+            return null;
+        }
+
+        [HttpGet("Parents/{Id}")]
+        public ActionResult<List<Node>> Parents(Guid Id)
+        {
+            //todo
+            return null;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] NodeViewModel nodeViewModel)
+        {
+            //todo
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return null;
+        }
+    }
+}
---
+++ FilesNFolders/WebAPI/ViewModels/NodeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using WebAPI.Models;
+
+namespace WebAPI.ViewModels
+{
+    public class NodeViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Path { get; set; }
+        public Guid? ParentId { get; set; }
+        [Required]
+        public NodeType Type { get; set; }
+    }
+}
